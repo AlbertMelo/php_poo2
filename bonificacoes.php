27285ac13774f24722166096c469e9fb261e658a
albert-melo <aldm.cid20@uea.edu.br>
2021-12-26 11:08:32 -0300

    PHP POO Avancando - Interfaces

--- bonificacoes.php
+++ bonificacoes.php
@@ -5,21 +5,25 @@ use Alura\Banco\Modelo\Funcionario\ {
     Diretor,
     Funcionario,
     Gerente,
-    Desenvolvedor
+    Desenvolvedor,
+    EditorVideo
 };
 use Alura\Banco\Service\ControladorDeBonificacoes;
 use Alura\Banco\Modelo\CPF;
 
-$umFuncionario = new Desenvolvedor('Vinicius Dias', new CPF('123.456.789-10'), 'Desenvolvedor', 1000);
+$umFuncionario = new Desenvolvedor('Vinicius Dias', new CPF('123.456.789-10'),  1000);
 $umFuncionario->sobeDeNivel();
 
-$umaFuncionaria = new Gerente('Patricia', new CPF('987.654.321-10'), 'Gerente', 3000);
+$umaFuncionaria = new Gerente('Patricia', new CPF('987.654.321-10'), 3000);
 
-$umDiretor = new Diretor('Ana Paula', new CPF('123.951.789-11'), 'Diretor', 5000);
+$umDiretor = new Diretor('Ana Paula', new CPF('123.951.789-11'),  5000);
+
+$umEditor = new EditorVideo('Valeria', new CPF('777.951.789-11'),  2000);
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
 $controlador->adicionaBonificacaoDe($umDiretor);
+$controlador->adicionaBonificacaoDe($umEditor);
 
 echo $controlador->recuperaTotal();
--- src/Modelo/Conta/Titular.php
+++ src/Modelo/Conta/Titular.php
@@ -1,14 +1,12 @@
 <?php
-
-
 namespace Alura\Banco\Modelo\Conta;
-
 
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Endereco;
 use Alura\Banco\Modelo\Pessoa;
+use Alura\Banco\Modelo\Autenticavel;
 
-class Titular extends Pessoa
+class Titular extends Pessoa implements Autenticavel
 {
 
     private Endereco $endereco;
@@ -22,5 +20,10 @@ class Titular extends Pessoa
     public function recuperaEndereco(): Endereco
     {
         return $this->endereco;
+    }
+
+    public function podeAutenticar(string $senha): bool
+    {
+        return $senha === 'abcd';
     }
 }
